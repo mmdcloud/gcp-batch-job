88015b87dfc2b151642d482b44c11f96a0fce207
mmdcloud <madmaxcloudpractice@gmail.com>
2025-05-02 06:25:14 +0530

    update: removed final-code from terraform

--- terraform/final-code/artifact_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir final-code
-cp -r ../code/* final-code/
-cd final-code
-docker buildx build --tag $1 --file ./Dockerfile .
-docker tag $1:latest $2-docker.pkg.dev/$3/$1/$1:latest
-docker push $2-docker.pkg.dev/$3/$1/$1:latest
