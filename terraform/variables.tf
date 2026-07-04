44230bdb5e2a22e4798f2d4610b114a3cac17cd1
mmdcloud <madmaxcloudpractice@gmail.com>
2026-07-04 09:21:44 +0530

    update: remove default values from variables in terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,15 @@
 variable "location" {
   type    = string
-  default = "us-central1"
 }
 
 variable "project_id" {
   type    = string
-  default = "encoded-alpha-457108-e8"
 }
 
 variable "repository_id" {
   type    = string
-  default = "batchnews"
 }
 
 variable "repository_description" {
   type    = string
-  default = "batchnews"
 }
